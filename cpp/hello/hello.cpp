5b6450e1601ae8d36d20f0835044d661941bd74c
wz <1208827324@qq.com>
2024-05-13 16:44:27 +0800

    cpp hello

---
+++ cpp/hello/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char const *argv[])
+{
+    
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    return 0;
+}
+
